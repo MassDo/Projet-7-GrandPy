896f7437b58bcd1278de1246644945488ce1ca8d
Dorian Massoulier <dorian_massoulier@hotmail.com>
2019-08-06 18:29:17 +0200

    dev front

--- app/static/test.js
+++ app/static/test.js
@@ -1,16 +1,40 @@
 // adding the new chat buble text ! 
-/*document.getElementById("ajaxButton").onclick = function() {
+document.getElementById("ajaxButton").onclick = function() {
+
+    // USER INPUT
     var userInput = document.getElementById("ajaxTextbox").value;
-    // creation of a new div in div chatZone and adding css class
-    var newDiv = document.createElement("div");
-    newDiv.classList.add("chatBuble")
-    // adding user text
-    newDiv.innerHTML = userInput;
-    // append this new div into the div 'chatZone'
-    document.getElementById("chatZone").appendChild(newDiv)    
-    // suppression of the text into id="ajaxTextbox"
-    document.getElementById("ajaxTextbox").value = ""    
-}*/
+
+    // NEW BUBBLE 
+    var msgDiv = document.createElement("div");
+    msgDiv.classList.add("msg");
+    document.getElementById("chat").appendChild(msgDiv);
+
+    var bubbleAltDiv = document.createElement("div");
+    bubbleAltDiv.className = "bubble alt";
+    msgDiv.appendChild(bubbleAltDiv);
+
+    var txtDiv = document.createElement("div");
+    txtDiv.classList.add("txt");
+    bubbleAltDiv.appendChild(txtDiv);
+
+    var meSpan = document.createElement("span");
+    meSpan.className = "name alt";
+    txtDiv.appendChild(meSpan);
+    meSpan.innerHTML = "Moi"
+
+    var timeSpan = document.createElement("span");
+    timeSpan.classList.add("timestamp");
+    txtDiv.appendChild(timeSpan);
+    timeSpan.innerHTML = "10:44 pm"
+
+    var messP = document.createElement("p");
+    messP.classList.add("message");
+    txtDiv.appendChild(messP);
+    messP.innerHTML = userInput; 
+
+    // CLEAR INPUT
+    document.getElementById("ajaxTextbox").value = "";
+}
 // resizing the textarea
 jQuery.each(jQuery('textarea[data-autoresize]'), function() {
   var offset = this.offsetHeight - this.clientHeight;
