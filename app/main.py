14d3cb91369f2d779826e4ffb061184836f0926f
Dorian Massoulier <dorian_massoulier@hotmail.com>
2019-08-16 20:06:55 +0200

    template

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ def home():
             lng=chatbot.longitude
         )
     else:
-        return render_template('/magicalgrandpy.herokuapp.com/') 
+        return render_template('/form.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
